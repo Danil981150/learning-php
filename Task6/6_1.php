e950f24ed2343394bbc7da436512ce902750a927
Danil981150 <danil981150@gamil.com>
2022-04-04 22:33:32 +0500

    rename foldres and task completion

---
+++ Task6/6_1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Мини - сайт, математика сложение чисел 5 и 3</title>
+</head>
+<body>
+    <h1>Математика сложение чисел 5 и 3</h1>
+    <ul>
+        <li><a href="addition.php">Сложение</a></li>
+        <li><a href="subtraction.php">Вычетание</a></li>
+        <li><a href="multiply.php">Умножение</a></li>
+        <li><a href="divide.php">Деление</a></li>
+    </ul>
+</body>
+</html>
---
+++ Task6/addition.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>математика сложение чисел 5 и 3</title>
+</head>
+<body>
+    <h1>Сложение чисел 5 и 3</h1>
+    <p>5 + 3 = <?= 5 + 3?></p>
+</body>
+</html>
